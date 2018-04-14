e6622602a56703a84a4e867578c89e3ce22998e3
David Goldenberg <dgoldenberg@dealerrater.com>
2018-04-14 15:53:24 -0400

    upgraded the firmware and now it's working, go figure

--- CoolerMasterSDKWrapper/Program.cs
+++ CoolerMasterSDKWrapper/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using c = CoolerMasterSDKWrapper.CoolerMasterExported;
 
 namespace CoolerMasterSDKWrapper
 {
@@ -6,7 +8,25 @@ namespace CoolerMasterSDKWrapper
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const string not = "not ";
+            var keyboard = DEVICE_INDEX.DEV_MK750;
+            c.SetControlDevice(keyboard);
+            if (!c.IsDevicePlug()) {
+                Console.WriteLine("Count not connect to the keyboard");
+                return;
+            }
+
+            if (!c.EnableLedControl(true, keyboard))
+            {
+                Console.WriteLine("Count not secure LED control for the keyboard");
+                return;
+            }
+
+            c.SetFullLedColor(0, 255, 0);
+
+            Thread.Sleep(1000);
+
+            c.EnableLedControl(false, keyboard);
         }
     }
 }
